5addc9fde0aefd84c8052cfb8d8a1c68bd445593
kiparenkomb <kiparenkomb@gmail.com>
2016-08-06 14:36:20 +0300

    new page

---
+++ My job/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+ /* use slicknav if bootstrap defult mobile nav not use */	
+
+
+
+ /***************** Smooth Scrolling ******************/
+
+
+
+ /***************** jquery scrool spy ******************/
+
+
+
+ /***************** prallax with scrolly ******************/
+
+  	$('.parallax_bg').scrolly({bgParallax: true});
+
+ /***************** wow animation activation ******************/
+
+ 	new WOW().init();
+
+ /***************** bootstrap mobile menu coustomized ******************/
+
+	$('#menu').slicknav({
+		label : ""		
+	});
+
+
+
+});
+
+
+
+
+
